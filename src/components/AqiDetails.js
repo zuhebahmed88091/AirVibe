31de2bb7b328c11cdc713e8aea3b76c281c16f02
Zuheb Ahmed <zuhebahmed88091@gmail.com>
2023-08-01 18:50:26 +0600

    Styled detailed page

--- src/components/AqiDetails.js
+++ src/components/AqiDetails.js
@@ -3,14 +3,14 @@ const AqiDetails = (cityId, cityData) => {
   if (cityArr.length > 0) {
     const components = Object.values(cityArr[0].data.components);
     const newComponents = [
-      { dataname: 'Carbon monoxide(CO)', data: '' },
-      { dataname: 'Nitrogen monoxide(NO)', data: '' },
-      { dataname: 'Nitrogen dioxide(NO₂)', data: '' },
-      { dataname: 'Ozone(O₃)', data: '' },
-      { dataname: 'Sulphur dioxide(SO₂)', data: '' },
-      { dataname: 'Fine particles matter(PM₂.₅)', data: '' },
-      { dataname: 'Coarse particulate matter(PM₁₀)', data: '' },
-      { dataname: 'Ammonia(NH₃)', data: '' },
+      { dataname: 'CO', data: '' },
+      { dataname: 'NO', data: '' },
+      { dataname: 'NO₂', data: '' },
+      { dataname: 'O₃', data: '' },
+      { dataname: 'SO₂', data: '' },
+      { dataname: 'PM₂.₅', data: '' },
+      { dataname: 'PM₁₀', data: '' },
+      { dataname: 'NH₃', data: '' },
     ];
     const updatedComponents = newComponents.map((component, index) => ({
       ...component,
