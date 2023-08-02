1849b7fb538cb47178f677fdb4b569bd86ecd4e0
Zuheb Ahmed <zuhebahmed88091@gmail.com>
2023-08-02 20:24:33 +0600

    Add filter search box

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,12 +1,15 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import logo from './Assets/weather-app.png';
 import continents from './Assets/continents.png';
+import Filter from './Filter';
 
-const Nav = () => (
+const Nav = ({ handleFilterChange }) => (
   <>
     <nav className="navbar">
       <img className="logo" src={logo} alt="logo" />
       <h1>AirVibe</h1>
+      <Filter handleFilterChange={handleFilterChange} />
     </nav>
     <header className="map-header">
       <div className="map">
@@ -15,7 +18,10 @@ const Nav = () => (
       <h1>Air Quality in Global Cities</h1>
     </header>
   </>
-
 );
+
+Nav.propTypes = {
+  handleFilterChange: PropTypes.func.isRequired,
+};
 
 export default Nav;
--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { getCitiesWeather } from '../redux/weatherSlice';
@@ -12,17 +12,28 @@ const Weather = () => {
   const isDataFetched = useSelector((state) => state.weather.isDataFetched);
   const dispatch = useDispatch();
 
+  const [filteredCities, setFilteredCities] = useState(cityData);
+  /* eslint-disable max-len */
+  const handleFilterChange = (filterText) => {
+    const filtered = cityData.filter((city) => city.city.toLowerCase().includes(filterText.toLowerCase()));
+    setFilteredCities(filtered);
+  };
+
   useEffect(() => {
     if (isDataFetched === false) {
       dispatch(getCitiesWeather());
     }
   }, [dispatch, isDataFetched]);
 
+  useEffect(() => {
+    setFilteredCities(cityData);
+  }, [cityData]);
+
   return (
     <div id="main-wrap">
-      <Nav />
+      <Nav handleFilterChange={handleFilterChange} />
       <section className="cities">
-        {cityData.map((city) => (
+        {filteredCities.map((city) => (
           <Link to={`city/${city.id}`} key={city.id}>
             <div className="arrow-img"><img className="arrow" src={arrow} alt="arrow" /></div>
             <Cities
